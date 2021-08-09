02360fc8a3e5cf6e98d90c89dd691ad3377d5fee
AIE\s210559 <s210559@students.aie.edu.au>
2021-08-09 16:57:40 +0930

    on trigger script, on clear script, detective screen trigger made

---
+++ NarrativeDesign/Assets/Scripts/OnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTrigger : MonoBehaviour
+{
+    public UnityEvent OnEnter;
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnter.Invoke();
+    }
+}
---
+++ NarrativeDesign/Assets/Scripts/clearroom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class clearroom : MonoBehaviour
+{
+    private bool sink, sauce, toilet, bath, soap, crayons;
+    public UnityEvent OnBathroomClear;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (sink == true && sauce == true && bath == true && crayons == true && toilet == true && soap == true)
+        {
+            OnBathroomClear.Invoke();
+        }
+    }
+
+    public void Sink()
+    {
+        sink = true;
+    }
+    public void Sauce()
+    {
+        sauce = true;
+    }
+    public void Bath()
+    {
+        bath = true;
+    }
+    public void Crayons()
+    {
+        crayons = true;
+    }
+    public void Toilet()
+    {
+        toilet = true;
+    }
+    public void Soap()
+    {
+        soap = true;
+    }
+}
