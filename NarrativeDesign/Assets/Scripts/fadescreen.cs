0161e336b4e33e375741cb0cbc3f91b96687fa93
AIE\s210559 <s210559@students.aie.edu.au>
2021-08-09 10:07:31 +0930

    intro dialogue, crying sound, broken fade script, crying fbx

---
+++ NarrativeDesign/Assets/Scripts/fadescreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class fadescreen : MonoBehaviour
+{
+    public Image image;
+    public float fadeSpeed = 1.5f;
+   
+    void Start()
+    {
+        image.color = Color.Lerp(image.color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+
+    
+}
