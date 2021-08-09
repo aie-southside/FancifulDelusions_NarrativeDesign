d69a6c8edafaa28941794d569e85fca1912d1168
AIE\s210559 <s210559@students.aie.edu.au>
2021-08-09 16:22:34 +0930

    made journal, bathroom clues

---
+++ NarrativeDesign/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+
+{
+    public GameObject journalScreen;
+    public GameObject miniMenu;
+    public GameObject howToPlay;
+    public string mainMenu;
+    public GameObject player;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            journalScreen.SetActive(true);
+            //Cursor.lockState = CursorLockMode.None;
+            //player.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            miniMenu.SetActive(true);
+        }
+    }
+
+    public void HowToPlayMenu()
+    {
+        howToPlay.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
