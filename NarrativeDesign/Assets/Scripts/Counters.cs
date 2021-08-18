ea0f0bfa69affcb9ee29f921f6ed3fb81910cb04
AIE\s210559 <s210559@students.aie.edu.au>
2021-08-18 17:02:13 +0930

    added counters

---
+++ NarrativeDesign/Assets/Scripts/Counters.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Counters : MonoBehaviour
+{
+    private TextMeshProUGUI tomatoCounter;
+
+
+
+   public void editTomatoCounter()
+    {
+        tomatoCounter.text = GetComponent<DetectiveTrigger>().wordCheck;
+    }
+    public void editCleanCounter()
+    {
+
+    }
+    public void editCrayonCounter()
+    {
+
+    } 
+    public void editLaughCounter()
+    {
+
+    }
+    public void editBathroomEvidenceCounter()
+    {
+
+    } 
+
+    public void editSpyEvidenceCounter()
+    {
+
+    }
+    
+
+}
